b567784b58d6067bd1f64827c58a5052bfa8a9c1
snakyhuman <snakyhuman@gmail.com>
2018-09-08 22:45:37 +0300

    Description: First Commit. Added projects, emplemented part of mine class Reviewer: NikolayM Final commit (Y/N): N

---
+++ ParserLib/MarketModel.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserLib
+{
+    public class MarketItem
+    {
+        #region Properties
+        string Model { get; set; } // //
+        string Name { get; set; } // 
+        string Description { get; set; } //
+        string Meta_description { get; set; } //
+        string Meta_title { get; set; } //
+        string Meta_keywords { get; set; } //
+        string SEO_url { get; set; } //
+        string Tags { get; set; } //
+        string SKU { get; set; } //
+        string EAN { get; set; } //
+        string UPC { get; set; } //
+        string JAN { get; set; } //
+        string MPN { get; set; } //
+        string ISBN { get; set; } //
+        string Quantity { get; set; } //
+        string Minimum { get; set; } //
+        string Subtract { get; set; } //
+        string Out_stock_status { get; set; } //
+        string Option { get; set; } //
+        string Option_type { get; set; } //
+        string Option_value { get; set; } //
+        string Option_required { get; set; } //
+        string Option_subtract { get; set; } //
+        string Option_image { get; set; } //
+        string Option_price_prefix { get; set; } //
+        string Option_points_prefix { get; set; } //
+        string Option_weight_prefix { get; set; } //
+        string Option_SKU_Options_Boost { get; set; } //
+        string Option_image_Options_Boost { get; set; } //
+        string Price { get; set; } //
+        string Spe_Customer_Group { get; set; } //
+        string Spe_Priority { get; set; } //
+        string Spe_Price { get; set; } //
+        string Spe_Date_start { get; set; } //
+        string Spe_Date_end { get; set; } //
+        string Dis_Customer_Group_1 { get; set; } //
+        string Dis_Quantity_1 { get; set; } //
+        string Dis_Priority_1 { get; set; } //
+        string Dis_Price_1 { get; set; } //
+        string Dis_Date_start_1 { get; set; } //
+        string Dis_Date_end_1 { get; set; } //
+        string Dis_Customer_Group_2 { get; set; } //
+        string Dis_Quantity_2 { get; set; } //
+        string Dis_Priority_2 { get; set; } //
+        string Dis_Price_2 { get; set; } //
+        string Dis_Date_start_2 { get; set; } //
+        string Dis_Date_end_2 { get; set; } //
+        string Dis_Customer_Group_3 { get; set; } //
+        string Dis_Quantity_3 { get; set; } //
+        string Dis_Priority_3 { get; set; } //
+        string Dis_Price_3 { get; set; } //
+        string Dis_Date_start_3 { get; set; } //
+        string Dis_Date_end_3 { get; set; } //
+        string Manufacturer { get; set; } //
+        string Cat_level_1 { get; set; } //
+        string Cat_level_2 { get; set; } //
+        string Cat_level_3 { get; set; } //
+        string Cat_level_4 { get; set; } //
+        string Cat_level_5 { get; set; } //
+        string Main_image { get; set; } //
+        string Image_2 { get; set; } //
+        string Image_3 { get; set; } //
+        string Image_4 { get; set; } //
+        string Image_5 { get; set; } //
+        string Date_available { get; set; } //
+        string Points { get; set; } //
+        string Points_Default { get; set; } //
+        string Requires_shipping { get; set; } //
+        string Location { get; set; } //
+        string Tax_class { get; set; } //
+        string Sort_order { get; set; } //
+        string Store { get; set; } //
+        string Status { get; set; } //
+        string Class_weight { get; set; } //
+        string Weight { get; set; } //
+        string Class_length { get; set; } //
+        string Length { get; set; } //
+        string Width { get; set; } //
+        string Height { get; set; } //
+        string Attr_Group_1 { get; set; } //
+        string Attribute_1 { get; set; } //
+        string Attribute_value_1 { get; set; } //
+        string Attr_Group_2 { get; set; } //
+        string Attribute_2 { get; set; } //
+        string Attribute_value_2 { get; set; } //
+        string Attr_Group_3 { get; set; } //
+        string Attribute_3 { get; set; } //
+        string Attribute_value_3 { get; set; } //
+        string Attr_Group_4 { get; set; } //
+        string Attribute_4 { get; set; } //
+        string Attribute_value_4 { get; set; } //
+        string Attr_Group_5 { get; set; } //
+        string Attribute_5 { get; set; } //
+        string Attribute_value_5 { get; set; } //
+        string Attr_Group_6 { get; set; } //
+        string Attribute_6 { get; set; } //
+        string Attribute_value_6 { get; set; } //
+        string Filter_Group_1 { get; set; } //
+        string Filter_Gr1_filter_1 { get; set; } //
+        string Filter_Gr1_filter_2 { get; set; } //
+        string Filter_Gr1_filter_3 { get; set; } //
+        string Filter_Group_2 { get; set; } //
+        string Filter_Gr2_filter_1 { get; set; } //
+        string Filter_Gr2_filter_2 { get; set; } //
+        string Filter_Gr2_filter_3 { get; set; } //
+        string Filter_Group_3 { get; set; } //
+        string Filter_Gr3_filter_1 { get; set; } //
+        string Filter_Gr3_filter_2 { get; set; } //
+        string Filter_Gr3_filter_3 { get; set; } //
+        string Layout { get; set; } //
+        #endregion
+    }
+}
